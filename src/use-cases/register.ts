c757f76b313121850b2bee14e59f961efe45d900
Jefferson Silva <jeffsilva01.dev@gmail.com>
2023-07-06 17:05:55 -0300

    Caso de Uso e Design Patterns - Handler de erros global

---
+++ src/use-cases/register.ts
@@ -0,0 +1,26 @@
+import { prisma } from '@/lib/prisma'
+import { usersRepository } from '@/repositories/users-repository'
+import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists'
+
+interface RegisterUseCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+export class RegisterUseCase {
+  constructor(private usersRepository: usersRepository) {}
+
+  async execute({ name, email, password }: RegisterUseCaseRequest) {
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
+
+    if (userWithSameEmail) {
+      throw new UserAlreadyExistsError()
+    }
+
+    const password_hash = await hash(password, 6)
+
+    await this.usersRepository.create({ name, email, password_hash })
+  }
+}
