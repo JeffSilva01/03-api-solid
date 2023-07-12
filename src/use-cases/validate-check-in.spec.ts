f7ea8142bdaddb032ba868aef2081a77fcd4c628
Jefferson Silva <jeffsilva01.dev@gmail.com>
2023-07-12 14:43:31 -0300

    Controllers & Testes E2E - Repositórios do Prisma

--- src/use-cases/validate-check-in.spec.ts
+++ src/use-cases/validate-check-in.spec.ts
@@ -1,7 +1,8 @@
-import { describe, it, expect, beforeEach } from 'vitest'
+import { describe, it, expect, beforeEach, vi, afterEach } from 'vitest'
 import { InMemoreCheckInsRepository } from '@/repositories/in-memore/in-memore-check-ins-repository'
 import { ValidateCheckInsCase } from './validate-check-in'
 import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { LateCheckInValidationError } from './errors/late-check-in-validation-error'
 
 let checkInsRepository: InMemoreCheckInsRepository
 let sut: ValidateCheckInsCase
@@ -10,6 +11,12 @@ describe('Validate Check In Use Case', () => {
   beforeEach(() => {
     checkInsRepository = new InMemoreCheckInsRepository()
     sut = new ValidateCheckInsCase(checkInsRepository)
+
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
   })
 
   it('should be able to validate the check-in', async () => {
@@ -31,5 +38,24 @@ describe('Validate Check In Use Case', () => {
         checkInId: 'id-not-exist',
       }),
     ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+
+  it('should not be able to validate the check-in after 20 minutes of its creation', async () => {
+    vi.setSystemTime(new Date(2023, 0, 1, 13, 40))
+
+    const createdCheckIn = await checkInsRepository.create({
+      gym_id: 'gym-01',
+      user_id: 'user-01',
+    })
+
+    const TWENTY_ONE_MINUTES_IN_MILLISECONDS = 1000 * 60 * 21
+
+    vi.advanceTimersByTime(TWENTY_ONE_MINUTES_IN_MILLISECONDS)
+
+    await expect(
+      sut.execute({
+        checkInId: createdCheckIn.id,
+      }),
+    ).rejects.toBeInstanceOf(LateCheckInValidationError)
   })
 })
